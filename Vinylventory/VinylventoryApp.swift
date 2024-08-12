94b9845ccca683d3cddd96d91515e10ce8824505
Tom Andrivet <tom@andrivet.com>
2024-08-12 15:07:43 +0200

    Hello World

---
+++ Vinylventory/VinylventoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  VinylventoryApp.swift
+//  Vinylventory
+//
+//  Created by Tom Andrivet on 12.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct VinylventoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
